4593fd7c8e047fd58594cfad1014b3a05bcf0963
Tim Brooks <tim.brooks@staples.com>
2014-08-06 20:42:28 -0500

    Initial implementation of tree in map form

--- src/trees/tree.clj
+++ src/trees/tree.clj
@@ -1,7 +1,19 @@
 (ns trees.tree)
 
-(defn- create-node [{:keys [condition leaf? children value] :as node-map}]
-  )
+(set! *warn-on-reflection* true)
 
-(defn tree [nodes]
-  (mapv create-node nodes))
+; not leaf: [leaf? condition children]
+; leaf: [leaf? value]
+(defn- create-node [{:keys [condition leaf? children value] :as node-map}]
+  (if leaf?
+    (to-array [true value])
+    (to-array [false condition (to-array children)])))
+
+(defn tree [node-maps]
+  (let [nodes (mapv create-node node-maps)]
+    {:nodes (to-array nodes)
+     :size (count nodes)
+     :condition-func (fn [^objects node] (aget node 1))
+     :value-func (fn [^objects node] (aget node 1))
+     :leaf-func (fn [^objects node] (aget node 0))
+     :children-func (fn [^objects node] (aget node 0))}))
