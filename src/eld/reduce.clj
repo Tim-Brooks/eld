d323dcf1a5ea97a96ebaf32ddd5879a6608625ce
Tim Brooks <tim.brooks@staples.com>
2014-08-17 00:32:04 -0700

    WIP: continued implementing reduce functionality

--- src/eld/reduce.clj
+++ src/eld/reduce.clj
@@ -4,20 +4,35 @@
 
 (set! *warn-on-reflection* true)
 
-(defn loop [node features new-tree-features new-tree nodes-to-search]
-  (cond (node/leaf? node)
-        (if (empty? nodes-to-search)
-          new-tree
-          nil)                                              ;; Add Node
+(defn add-node [node new-tree parent-stack]
+  (if (empty? parent-stack)
+    (conj new-tree node)
+    (let [^objects parent-pair (peek parent-stack)
+          new-children (aget parent-pair 1)]
+      (if (== (aget parent-pair 0) (count new-children))
+        (aset ))
+      )))
 
-        (contains? new-tree-features (node/feature node))
-        nil                                                 ;; Add search direction
+;; Hardcoded root right now
+;; Slow stacks
+;; new tree is vector
+(defn reduce-tree [tree features new-tree-features]
+  (let [new-tree []]
+    (loop [node (tree/get-node tree 0)
+           nodes-to-search []
+           parent-stack []]
+      (cond (node/leaf? node)
+            (if (empty? nodes-to-search)
+              (conj new-tree node)
+              (conj new-tree node))                                          ;; Add Node
 
-        :else
-        (recur (tree/get-node (node/next-node-id node features))
-               features
-               new-tree-features
-               new-tree
-               nodes-to-search)))
+            (contains? new-tree-features (node/feature node))
+            nil                                             ;; Add search direction
 
-(defn reduce-tree [tree features new-tree-features])
+            :else
+            (recur (tree/get-node (node/next-node-id node features))
+                   features
+                   new-tree-features
+                   new-tree
+                   nodes-to-search))
+      (recur node nodes-to-search parent-stack))))
