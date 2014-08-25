685299df7403c3a018517455ec4c1caa29da1210
Tim Brooks <tim.brooks@staples.com>
2014-08-24 22:18:36 -0700

    Add documentation for core functions

--- src/eld/core.clj
+++ src/eld/core.clj
@@ -3,11 +3,13 @@
             [eld.implementation.vector :as vector]))
 
 (defn array-tree [node-maps]
+  "Returns an eld tree whose nodes are represented by object arrays."
   (let [tree (array/create-tree node-maps)]
     {:nodes tree
      :root  0}))
 
 (defn vector-tree [node-maps]
+  "Returns an eld tree whose nodes are represented by persistent vectors."
   (let [tree (vector/create-tree node-maps)]
     {:nodes tree
      :root  0}))
