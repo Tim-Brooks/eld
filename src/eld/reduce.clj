f7d081b7999a8a459ddf000bca2b05fd727b423d
Tim Brooks <tim.brooks@staples.com>
2014-08-27 20:59:37 -0700

    WIP: Continued implementing reduce functionality

--- src/eld/reduce.clj
+++ src/eld/reduce.clj
@@ -1,35 +1,48 @@
 (ns eld.reduce
   (:require [eld.node :as node]
-            [eld.tree :as tree]))
+            [eld.tree :as tree])
+  (:import (java.util Stack)))
 
 (set! *warn-on-reflection* true)
 
-(defn add-node [node new-tree parent-stack]
-  (if (empty? parent-stack)
-    (conj new-tree node)
-    (let [^objects parent-pair (peek parent-stack)
-          new-children (aget parent-pair 1)]
-      (if (== (aget parent-pair 0) (count new-children))
-        nil
-        nil))))
+(defn- add-node [node new-tree ^Stack parent-stack]
+  (let [^objects parent-pair (.pop parent-stack)
+        ^objects new-children (aget parent-pair 2)
+        child-idx (aget parent-pair 1)
+        ^int idx (aget parent-pair 0)]
+    (aset new-children idx (count new-tree))
+    (if (== child-idx (alength new-children))
+      (do (node/set-children! (nth new-tree idx) new-children)
+          (conj! new-tree node))
+      (conj! new-tree node))))
 
-;; Hardcoded root right now
-;; Slow stacks
+(defn- add-to-search [parent-idx ^objects children ^Stack nodes-to-search ^Stack parent-stack]
+  (loop [i 0 [child & rest] children]
+    (.push nodes-to-search child)
+    (.push parent-stack (object-array [parent-idx i (object-array (alength children))]))
+    (recur (inc i) rest)))
+
 ;; new tree is vector
-(defn reduce-tree [{:keys [nodes]} features new-tree-features]
-  (let [new-tree []]
-    (loop [node (tree/get-node nodes 0)
-           nodes-to-search []
-           parent-stack []]
+(defn reduce-tree [{:keys [nodes root]} features new-tree-features]
+  (loop [node-id root
+         new-tree (transient [])
+         nodes-to-search (Stack.)
+         parent-stack (Stack.)]
+    (let [node (tree/get-node nodes node-id)]
       (cond (node/leaf? node)
             (if (empty? nodes-to-search)
-              (conj new-tree node)
-              (conj new-tree node))                                          ;; Add Node
+              (persistent! (conj! new-tree node))
+              (let [next (.pop nodes-to-search)]
+                (recur next                                 ;; Assumes real node
+                       (add-node node new-tree parent-stack)
+                       nodes-to-search
+                       parent-stack)))
 
             (contains? new-tree-features (node/feature node))
-            nil                                             ;; Add search direction
+            nil
 
             :else
-            (recur (tree/get-node nodes (node/next-node-id node features))
+            (recur (node/next-node-id node features)
+                   new-tree
                    nodes-to-search
                    parent-stack)))))
